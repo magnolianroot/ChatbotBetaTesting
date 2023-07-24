9acec009a52c04eeaf49897a14df9fce7a2cd033
Victoria Joseph <110946840+magnolianroot@users.noreply.github.com>
2023-07-24 10:58:00 -0400

    Update ChatbotBetaTesting.py

--- ChatbotBetaTesting.py
+++ ChatbotBetaTesting.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "..." #stored in original source file
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
 print(completion.choices[0].message.content)
